52b8316d4920c567be145191cbe84eb91dfa7d42
bilal afzaal <softnacuk@gmail.com>
2022-02-04 01:06:04 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  env: {
+    MOVIE_API_KEY: "e539e847",
+  },
   reactStrictMode: true,
-}
+};
---
+++ pages/movie/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const id = () => {
+  return <div>single movie page</div>;
+};
+
+export default id;
