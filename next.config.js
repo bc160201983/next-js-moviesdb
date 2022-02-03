ca118970b5aa6e6156ca75b1b04764b395f83374
bilal afzaal <softnacuk@gmail.com>
2022-02-04 01:15:25 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   env: {
     MOVIE_API_KEY: "e539e847",
   },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
   reactStrictMode: true,
 };
